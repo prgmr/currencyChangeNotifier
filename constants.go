51e04f775ae3d488325610b1893c8c827ff894e7
dmitry <example.com>
2023-05-01 12:08:54 +0300

    update

--- constants.go
+++ constants.go
@@ -2,4 +2,4 @@ package main
 
 const CbrApiUrl string = "https://www.cbr-xml-daily.ru/daily_json.js"
 
-var defaultCurrenciesArray = [...]string{"USD", "EUR", "RUB"}
+var defaultCurrenciesArray = [...]string{"USD", "EUR"}
--- currency_change_notifier.go
+++ currency_change_notifier.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/akamensky/argparse"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"time"
 )
 
 func getValuesFromApi() {
@@ -23,24 +27,45 @@ func getValuesFromApi() {
 	}
 
 	for _, curr := range defaultCurrenciesArray {
-		z := result.Valute[curr].(map[string]interface{})[curr]
-		fmt.Println(z)
+		currency := result.Valute[curr]
+		msg := fmt.Sprintf("%s = %f RUB", curr, currency.Value)
+		showNotification(msg)
 	}
+}
 
+func showNotification(msg string) {
+	cmd := exec.Command("notify-send", msg)
+	_, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Notification error: " + err.Error())
+	}
+}
+
+func start(showError bool) {
+	getValuesFromApi()
 }
 
 func main() {
-	//parser := argparse.NewParser("currencyChangeNotifier", "Program notifies you about changes in the exchange rate")
-	//repeatApiRequestArg := parser.Int("r", "repeat", &argparse.Options{Required: false, Help: "Check currency data every some minutes. Default is 5 minutes", Default: 5})
-	////notifyAboutErrorsArg := parser.Flag("s", "show_error", &argparse.Options{Required: false, Help: "Notify about program errors", Default: false})
-	////currenciesListArg := parser.List("a", "additional_currencies", &argparse.Options{Required: false, Help: fmt.Sprintf("Additional list of currencies. Default are %s", strings.Join(defaultCurrenciesArray[:], ", ")), Default: defaultCurrenciesArray})
-	//
-	//err := parser.Parse(os.Args)
-	//if err != nil {
-	//	fmt.Print(parser.Usage(err))
-	//}
-	//// Finally print the collected string
-	//fmt.Println(*repeatApiRequestArg)
+	parser := argparse.NewParser("currencyChangeNotifier", "Program notifies you about changes in the exchange rate")
+	repeatApiRequestArg := parser.Int("r", "repeat", &argparse.Options{Required: false, Help: "Check currency data every some minutes. Default is 5 minutes", Default: 1})
+	notifyAboutErrorsArg := parser.Flag("s", "show_error", &argparse.Options{Required: false, Help: "Notify about program errors", Default: false})
+	//currenciesListArg := parser.List("a", "additional_currencies", &argparse.Options{Required: false, Help: fmt.Sprintf("Additional list of currencies. Default are %s", strings.Join(defaultCurrenciesArray[:], ", ")), Default: defaultCurrenciesArray})
 
-	getValuesFromApi()
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+	}
+
+	start(*notifyAboutErrorsArg)
+
+	ticker := time.NewTicker(time.Duration(*repeatApiRequestArg) * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			start(*notifyAboutErrorsArg)
+		}
+	}
+
 }
